687bc5b849c22cb9338cd7639b657e8862413e8f
Samuel Dagesse <sam.dag13@gmail.com>
2024-03-27 21:47:29 -0400

    Finalize build directory structure

--- src/Test.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-class Logger{
-    public:
-    void Print(std::string msg);    
-};
---
+++ src/log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+#include "kijin_export.h"
+
+namespace kijin
+{
+    KIJIN_API void Print(std::string msg);
+};
