98c8d9b1fbe9639de574cfcce9a8887e0b6add84
Samuel Dagesse <sam.dag13@gmail.com>
2024-03-29 08:15:15 -0400

    basic window code
    Basic glfw template code from the doc

--- src/window.cpp
+++ src/window.cpp
@@ -4,6 +4,8 @@
 struct pimpl
 {
     GLFWwindow *handle{nullptr};
+    int width;
+    int height;
 };
 
 window::window() : Pimpl(new pimpl)
@@ -13,4 +15,44 @@ window::window() : Pimpl(new pimpl)
 window::~window()
 {
     delete Pimpl;
+}
+
+bool window::init(int width, int height, const char *title, GLFWmonitor *monitor, GLFWwindow *share)
+{
+    if (!glfwInit())
+        return false;
+
+    Pimpl->width = width;
+    Pimpl->height = height;
+    Pimpl->handle = glfwCreateWindow(Pimpl->width, Pimpl->height, title, monitor, share);
+
+    if (!Pimpl->handle)
+        return false;
+
+    glfwMakeContextCurrent(Pimpl->handle);
+    glfwSetFramebufferSizeCallback(Pimpl->handle, fb_resize_callback);
+
+    return true;
+}
+
+// TODO: Load openGL functions with GLEW, include & compile glew
+void window::run()
+{
+    while (!glfwWindowShouldClose(Pimpl->handle))
+    {
+        /*Render here*/
+        // glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(Pimpl->handle);
+
+        // Event polling
+        glfwPollEvents();
+    }
+    glfwTerminate();
+}
+
+void window::fb_resize_callback(GLFWwindow *window, int width, int height)
+{
+    // glViewport(0, 0 width, height);
 }
--- src/window.h
+++ src/window.h
@@ -2,7 +2,12 @@
 
 #include "kijin_export.h"
 
+#include <array>
+#include <string>
+
 struct pimpl;
+class GLFWmonitor;
+class GLFWwindow;
 
 KIJIN_API class window
 {
@@ -10,9 +15,11 @@ public:
     window();
     ~window();
 
-    void init();
-    void execute();
+    bool init(int width, int height, const char *title, GLFWmonitor *monitor = nullptr, GLFWwindow *share = nullptr);
+    void run();
 
 private:
     pimpl *Pimpl;
+
+    static void fb_resize_callback(GLFWwindow *window, int width, int height);
 };
