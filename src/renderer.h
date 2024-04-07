4323c6ca9ae6b01ae71d25b70f70da329f90e32d
Samuel Dagesse <sam.dag13@gmail.com>
2024-04-07 11:27:46 -0400

    Add rendering classes form my other project

---
+++ src/renderer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <GL/glew.h>
+
+#include "vertex_array.h"
+#include "index_buffer.h"
+#include "shader.h"
+
+#ifdef OGL_DEBUG == 1
+#define LOG(x) std::cout << x << std::endl
+#define ASSERT(y) \
+    if (!(y))     \
+        __debugbreak();
+#define GLCall(x)   \
+    GLClearError(); \
+    x;              \
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#elif defined(OGL_RELEASE)
+#define GLCall(x) x
+#define LOG(x)
+#else
+#define LOG(x)
+#endif
+
+void AdjustIncrement(float &increment, float component);
+void GLClearError();
+bool GLLogCall(const char *function, const char *file, int line);
+
+class Renderer
+{
+
+private:
+public:
+    void Clear() const;
+    void Draw(const VertexArray &va, const IndexBuffer &ib, const Shader &shader) const;
+};
---
+++ src/vertex_array.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "vertex_buffer.h"
+
+class VertexBufferLayout;
+
+class VertexArray
+{
+private:
+	unsigned int m_RendererID;
+
+public:
+	VertexArray();
+	~VertexArray();
+	void Bind() const;
+	void Unbind() const;
+
+	void AddBuffer(const VertexBuffer &vb, const VertexBufferLayout &layout);
+};
