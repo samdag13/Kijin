79d49ce6823918abf76d5621d91b15212cf8c1ce
Samuel Dagesse <sam.dag13@gmail.com>
2024-04-28 12:34:28 -0400

    shader support vertex color

--- resources/shaders/basic_shader.glsl
+++ resources/shaders/basic_shader.glsl
@@ -1,17 +1,20 @@
 #shader vertex
 #version 330 core 
 
-layout(location = 0) in vec4 position;
-layout(location = 1) in vec2 texCoord;
+layout(location = 0) in vec4 aPosition;
+layout(location = 1) in vec4 aColor;
+layout(location = 2) in vec2 aTexCoord;
 
+out vec4 v_color;
 out vec2 v_TexCoord;
 
 uniform mat4 u_MVP;
 
 void main() 
 { 
-	gl_Position = u_MVP * position; 
-	v_TexCoord = texCoord;
+	gl_Position = u_MVP * aPosition; 
+	v_color = aColor;
+	v_TexCoord = aTexCoord;
 };
 
 
@@ -21,13 +24,13 @@ void main()
 
 layout(location = 0) out vec4 color; 
 
+in vec4 v_color;
 in vec2 v_TexCoord;
 
-//uniform vec4 u_Color;
 uniform sampler2D u_Texture;
 
 void main() 
 { 
-	vec4 texColor = texture(u_Texture, v_TexCoord);
-	color = texColor; 
+	vec4 frag_color = texture(u_Texture, v_TexCoord);
+	color = frag_color * v_color; 
 };
